fe55807c75c4a1d283fe27268e58b3476dcb3b7f
juakonap <juakonap@gmail.com>
2020-07-01 19:22:43 +0000

    UPDATE: configs y scrapers

--- src/config.py
+++ src/config.py
@@ -1,5 +1,6 @@
 
 url_base = 'https://www.dt.gob.cl/portal/1626/'
+url_multa = 'https://ventanilla.dirtrab.cl/RegistroEmpleador/consultamultas.aspx'
 
 driver_path = '/home/testmailjoaquin/meetup-webscraping/chromedriver'
 #driver_path = '/usr/local/bin/chromedriver'
--- src/dataEnrichment.py
+++ src/dataEnrichment.py
@@ -1,18 +1,3 @@
-import pandas as pd 
-import time
-from sii import info_sii
 
-dataset = pd.read_csv('../csv/compilado_ruts_unicos.txt', sep = '\n', header = None)
 
-data_sii = pd.DataFrame()
 
-for r in range(dataset.shape[0]):
-    try:
-        rut = dataset[0][r].split('-')
-        data_sii = data_sii.append(info_sii(rut[0],rut[1]), ignore_index = True)
-        time.sleep(2)
-    except:
-        pass
-    print(r)
-
-data_sii.to_csv('../csv/compilado_reducción_jornada_info_sii.csv', encoding = 'utf-8', index = False)
